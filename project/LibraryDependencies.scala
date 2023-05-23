b7260daeff9ecfb09a2f72aa24f960a6a941e8b6
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2023-05-23 11:51:46 +0100

    API-6296 - Bump domain lib version

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -6,7 +6,7 @@ object LibraryDependencies {
   lazy val compileDependencies = Seq(
     "com.typesafe.play"       %% "play-json"                        % "2.9.2",
     "org.typelevel"           %% "cats-core"                        % "2.9.0",
-    "uk.gov.hmrc"             %% "api-platform-application-domain"  % "0.11.0"
+    "uk.gov.hmrc"             %% "api-platform-application-domain"  % "0.12.0"
   )
 
   lazy val testDependencies = Seq(
